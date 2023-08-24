150381e79cee71362a12dfa2e2774d01d6c32b88
Mindaugas Žvirblis <mz@9g.lt>
2023-08-24 15:51:14 +0300

    something to check

---
+++ go.mod
@@ -0,0 +1,16 @@
+module nomad-netnpc
+
+go 1.21.0
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/cronexpr v1.1.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/nomad/api v0.0.0-20230823204213-f5b0da1d551d // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/exp v0.0.0-20230728194245-b0cb94b80691 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/hashicorp/nomad/api"
+)
+
+func main() {
+	me := os.Getenv("NOMAD_ALLOC_ID")
+
+	nc, err := api.NewClient(&api.Config{
+		Address:  os.Getenv("NOMAD_ADDR"),
+		SecretID: os.Getenv("NOMAD_TOKEN"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	alloc, _, err := nc.Allocations().Info(me, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("I am running on: %v", alloc.NodeName)
+
+	topics := map[api.Topic][]string{}
+	_ = topics
+	ctx := context.Background()
+	eventsClient := nc.EventStream()
+	eventCh, err := eventsClient.Stream(ctx, topics, 0, &api.QueryOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			os.Exit(0)
+
+		case event := <-eventCh:
+
+			if event.IsHeartbeat() {
+				continue
+			}
+
+			for _, task := range event.Events {
+				if task.Type == "ServiceRegistration" || task.Type == "ServiceDeregistration" {
+					serv, _ := task.Service()
+					sl, _, _ := nc.Services().Get(serv.ServiceName, &api.QueryOptions{})
+					for _, s := range sl {
+						alloc, _, err := nc.Allocations().Info(s.AllocID, &api.QueryOptions{})
+						if err != nil {
+							log.Printf("%v", err)
+							continue
+						}
+						if task.Type == "ServiceRegistration" {
+							log.Printf("iptables-add %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
+						} else {
+							log.Printf("iptables-remove %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
+						}
+					}
+				}
+			}
+		}
+	}
+}
