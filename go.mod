b10b772fd413b31e3c2a82cf35dadd831130d384
Mindaugas Žvirblis <mz@9g.lt>
2023-08-24 16:10:49 +0300

    checking docker output

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module nomad-netnpc
 go 1.21.0
 
 require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/docker v24.0.5+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/cronexpr v1.1.2 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
@@ -12,5 +18,12 @@ require (
 	github.com/hashicorp/nomad/api v0.0.0-20230823204213-f5b0da1d551d // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/exp v0.0.0-20230728194245-b0cb94b80691 // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/docker/docker/api/types"
+	dockerclient "github.com/docker/docker/client"
 	"github.com/hashicorp/nomad/api"
 )
 
@@ -38,35 +42,87 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for {
-		select {
-		case <-ctx.Done():
-			os.Exit(0)
+	go func() {
 
-		case event := <-eventCh:
+		for {
+			select {
+			case <-ctx.Done():
+				os.Exit(0)
 
-			if event.IsHeartbeat() {
-				continue
-			}
+			case event := <-eventCh:
 
-			for _, task := range event.Events {
-				if task.Type == "ServiceRegistration" || task.Type == "ServiceDeregistration" {
-					serv, _ := task.Service()
-					sl, _, _ := nc.Services().Get(serv.ServiceName, &api.QueryOptions{})
-					for _, s := range sl {
-						alloc, _, err := nc.Allocations().Info(s.AllocID, &api.QueryOptions{})
-						if err != nil {
-							log.Printf("%v", err)
-							continue
-						}
-						if task.Type == "ServiceRegistration" {
-							log.Printf("iptables-add %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
-						} else {
-							log.Printf("iptables-remove %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
+				if event.IsHeartbeat() {
+					continue
+				}
+
+				for _, task := range event.Events {
+					if task.Type == "ServiceRegistration" || task.Type == "ServiceDeregistration" {
+						serv, _ := task.Service()
+						sl, _, _ := nc.Services().Get(serv.ServiceName, &api.QueryOptions{})
+						for _, s := range sl {
+							alloc, _, err := nc.Allocations().Info(s.AllocID, &api.QueryOptions{})
+							if err != nil {
+								log.Printf("%v", err)
+								continue
+							}
+							if task.Type == "ServiceRegistration" {
+								log.Printf("iptables-add %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
+							} else {
+								log.Printf("iptables-remove %v %v %v", s.ServiceName, s.Address, alloc.NodeName)
+							}
 						}
 					}
 				}
 			}
 		}
+	}()
+
+	cli, err := dockerclient.NewClientWithOpts(dockerclient.FromEnv)
+	if err != nil {
+		panic(err)
 	}
+
+	ems, errs := cli.Events(context.Background(), types.EventsOptions{})
+	go func() {
+		for {
+			select {
+			case err := <-errs:
+				fmt.Println(err)
+			case em := <-ems:
+				if em.Type == "network" && (em.Action == "connect" || em.Action == "disconnect") {
+					info, err := cli.ContainerInspect(context.Background(), em.Actor.Attributes["container"])
+					if err != nil {
+						log.Printf("%v", err)
+					}
+					var allocID string
+					var nodeName string
+					for _, env := range info.Config.Env {
+						if strings.Contains(env, "NOMAD_ALLOC_ID") {
+							allocID = strings.Split(env, "=")[1]
+							log.Printf("NOMAD_ALLOC_ID: %s", allocID)
+						}
+					}
+
+					alloc, _, err := nc.Allocations().Info(allocID, nil)
+
+					if err != nil {
+						log.Printf("%v", err)
+					}
+					if err == nil {
+						nodeName = alloc.NodeName
+					}
+
+					log.Printf(
+						"Docker Event: Container %s %s to network %s ( alloc: %s) Node %s",
+						info.Name,
+						em.Action,
+						info.NetworkSettings.Networks[em.Actor.Attributes["name"]].IPAddress,
+						allocID,
+						nodeName,
+					)
+				}
+			}
+		}
+	}()
+	select {}
 }
